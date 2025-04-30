2e67cdee49cdcc40386ae9c353c2132b8ca6f766
Julien <julien@kalisikaran.com>
2025-04-30 16:19:27 +0000

    wip manual

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module gh-app
 
 go 1.24.1
-
-require (
-	github.com/google/go-github/v71 v71.0.0
-	golang.org/x/oauth2 v0.29.0
-)
-
-require github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -2,36 +2,77 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"net/url"
+	"net/http"
 	"os"
-
-	"github.com/google/go-github/v71/github"
-	"golang.org/x/oauth2"
 )
+
+type AppDetails struct {
+	AppId    int    `json:"id"`
+	Name     string `json:"name"`
+	Slug     string `json:"slug"`
+	ClientID string `json:"client_id"`
+	// Add other fields as needed
+}
+
+type GitHubClient struct {
+	BaseURL string
+	Token   string
+	Client  *http.Client
+}
+
+func NewGitHubClient(token string) *GitHubClient {
+	if token == "" {
+		log.Fatalf("token is not set (use GH_TOKEN env)")
+	}
+	return &GitHubClient{
+		BaseURL: "https://api.github.com/",
+		Token:   token,
+		Client:  &http.Client{},
+	}
+}
+
+func (gh *GitHubClient) Get(ctx context.Context, uri string, result interface{}) error {
+	apiURL := gh.BaseURL + uri
+	req, err := http.NewRequestWithContext(ctx, "GET", apiURL, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.Header.Set("Authorization", "Bearer "+gh.Token)
+	req.Header.Set("Accept", "application/vnd.github+json")
+
+	resp, err := gh.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error making request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(result); err != nil {
+		return fmt.Errorf("error decoding response: %w", err)
+	}
+
+	return nil
+}
 
 func main() {
 	ctx := context.Background()
-
 	token := os.Getenv("GH_TOKEN")
-	if token == "" {
-		log.Fatalf("GH_TOKEN environment variable is not set")
+	client := NewGitHubClient(token)
+
+	var appDetails AppDetails
+	if err := client.Get(ctx, "apps/four-wards", &appDetails); err != nil {
+		log.Fatalf("Error fetching app details: %v", err)
 	}
 
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-	var err error
-	client.BaseURL, err = url.Parse("http://localhost:8080/")
-	if err != nil {
-		log.Fatalf("Error parsing base URL: %v", err)
-	}
-
-	data, _, err := client.Apps.Get(ctx, "four-wards")
-	if err != nil {
-		log.Fatalf("Error fetching data: %v", err)
-	}
-
-	log.Printf("%v", data)
+	log.Printf("App ID: %d", appDetails.AppId)
+	log.Printf("App Name: %s", appDetails.Name)
+	log.Printf("App Slug: %s", appDetails.Slug)
+	log.Printf("Client ID: %s", appDetails.ClientID)
 }
