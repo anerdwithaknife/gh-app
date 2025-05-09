5aa901db4faea0460aaaa1adbf635ffd07cbc37e
Julien <julien@kalisikaran.com>
2025-05-09 17:26:07 +0000

    feat: Support org as alternative to installation id when generating token

--- cmd/token.go
+++ cmd/token.go
@@ -24,28 +24,51 @@ to see available installation ids for a given app slug.`,
 		}
 
 		slug, _ := cmd.Flags().GetString("slug")
+
 		if slug == "" {
 			log.Fatal("Slug must not be empty")
 		}
 
 		installationID, _ := cmd.Flags().GetString("installation-id")
-		if installationID == "" {
-			log.Fatal("Installation ID must not be empty")
+		orgName, _ := cmd.Flags().GetString("org-name")
+
+		if (installationID == "" && orgName == "") || (installationID != "" && orgName != "") {
+			log.Fatal("You must provide either --installation-id (-i) or --org-name (-o)")
 		}
 
 		app, err := db.GetAppBySlug(slug)
 		if err != nil {
-			log.Fatalf("Error getting app details: %v", err)
+			log.Fatalf("Error: Problem reading yaml store: %v", err)
+		}
+
+		if app == nil {
+			log.Fatal("Error: App was not found in yaml store")
 		}
 
 		jwtToken, err := github.GenerateGithubAppJWT(app.AppID, app.PrivateKey)
 		if err != nil {
-			log.Fatalf("Error generating JWT: %v", err)
+			log.Fatalf("Error: Problem generating JWT: %v", err)
+		}
+
+		if installationID == "" {
+			installations, err := github.GetAppInstallations(jwtToken, app.AppID)
+			if err != nil {
+				log.Fatalf("Error: Problem fetching installations: %v", err)
+			}
+			for _, installation := range installations {
+				if installation.Account.Login == orgName {
+					installationID = fmt.Sprintf("%d", installation.Id)
+					break
+				}
+			}
+			if installationID == "" {
+				log.Fatalf("Error: No installation found for org: %s", orgName)
+			}
 		}
 
 		accessToken, err := github.GenerateAccessToken(jwtToken, app.AppID, installationID)
 		if err != nil {
-			log.Fatalf("Error generating access token: %v", err)
+			log.Fatalf("Error: Problem generating access token: %v", err)
 		}
 
 		fmt.Println(accessToken)
@@ -57,7 +80,7 @@ func init() {
 
 	tokenCmd.Flags().StringP("slug", "s", "", "The slug of the app")
 	tokenCmd.Flags().StringP("installation-id", "i", "", "The associated installation id")
+	tokenCmd.Flags().StringP("org-name", "o", "", "The org to retrieve the installation id from")
 
 	tokenCmd.MarkFlagRequired("slug")
-	tokenCmd.MarkFlagRequired("installation-id")
 }
